4085f5986b94d29d9bbce9b68ce59acbd09abed3
Jeremy Kahn <jeremyckahn@gmail.com>
2012-12-30 10:04:26 -0800

    Adds areIntersecting method.

--- src/drawable.js
+++ src/drawable.js
@@ -26,6 +26,23 @@ define([
     this._width = 0
   }
 
+  _.extend(Drawable, {
+    /**
+     * @param {Drawable} drawable1
+     * @param {Drawable} drawable2
+     * @return {number}
+     */
+    areIntersecting: function (drawable1, drawable2) {
+      // Note: I'm lazy and stole the logic from here:
+      // http://stackoverflow.com/questions/2752349/fast-rectangle-to-rectangle-intersection/2752387#2752387
+      return !(drawable2._x > drawable1.getRight() ||
+             drawable2.getRight() < drawable1._x ||
+             drawable2.getTop() > drawable1._y ||
+             drawable2._y < drawable1.getTop())
+    }
+
+  })
+
   /**
    * @param {Viewport} viewport
    */
@@ -45,18 +62,11 @@ define([
   }
 
   Drawable.prototype = {
-    /**
-     * @abstract
-     */
-    draw: function () {
-      console.warn(
-          'Calling Drawable#draw. You probably meant to override this.')
-    }
 
     /**
      * @param {Viewport} viewport
      */
-    ,_applyViewportOffset: function (viewport) {
+    _applyViewportOffset: function (viewport) {
       this._y -= viewport.getYOffset()
     }
 
@@ -65,6 +75,28 @@ define([
      */
     ,_unapplyViewportOffset: function (viewport) {
       this._y += viewport.getYOffset()
+    }
+
+    /**
+     * @abstract
+     */
+    ,draw: function () {
+      console.warn(
+          'Calling Drawable#draw. You probably meant to override this.')
+    }
+
+    /**
+     * @return {number}
+     */
+    ,getTop: function () {
+      return this._y + this._height
+    }
+
+    /**
+     * @return {number}
+     */
+    ,getRight: function () {
+      return this._x + this._width
     }
   }
 
