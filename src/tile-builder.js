a5314d752eb48c8313f3e79c28a3563d32bd725e
Jeremy Kahn <jeremyckahn@gmail.com>
2012-12-30 21:33:48 -0800

    Adds getTileForPoint and some related properties.

--- src/tile-builder.js
+++ src/tile-builder.js
@@ -37,6 +37,18 @@ define([
   function TileRenderer (ctx, tileMap) {
     var tiles = []
 
+    /** @type {number} */
+    this._tileHeight = tileMap.tileHeight
+    /** @type {number} */
+    this._tileWidth = tileMap.tileWidth
+    /** @type {number} */
+    this._tileGridRows = tileMap.map.length
+    /** @type {number} */
+    this._tileGridColumns = tileMap.map[0].length
+
+    /** @type {Array.<Array.<TileCode>>} */
+    this._tiles = tileMap.map
+
     tileMap.map.reverse().forEach(function (row, y) {
       row.forEach(function (tileCode, x) {
         // NOTE: One could make a Tile Object (which would inherit from
@@ -65,6 +77,26 @@ define([
       this._drawables.forEach(function (drawable) {
         drawable.draw()
       })
+    }
+
+    /**
+     * @param {number} pointX
+     * @param {number} pointY
+     * @return {Tile|null} null is returned if there is no tile for pointX and
+     * pointY.
+     */
+    ,getTileForPoint: function (pointX, pointY) {
+      if (pointX < 0 || pointX > (this._tileHeight * this._tileGridRows)
+        || pointY < 0 || pointY > (this._tileWidth * this._tileGridColumns)) {
+        return null
+      }
+
+      var intPointX = parseInt(pointX, 10)
+      var intPointY = parseInt(pointY, 10)
+      var xCoord = parseInt(intPointX / this._tileWidth, 10)
+      var yCoord = parseInt(intPointY / this._tileHeight, 10)
+
+      return this._tile[yCoord][xCoord]
     }
   }
 
